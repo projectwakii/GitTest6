395a6c76b308a3617c6061f26a451d1be7711ab4
projectwakii <projectwakii@gmail.com>
2015-06-17 20:33:43 +0900

    Commit A of Branch 1 created.

--- GitTest6/ViewController.swift
+++ GitTest6/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print("This is commit A.")
+        
+        print("This is commit A of branch 1.")
     }
 
     override func didReceiveMemoryWarning() {
